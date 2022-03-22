b3d213c5961f950a8ebbb3ea35a914b53f909650
andikarafon <andika.rafon.senohaji@gmail.com>
2022-03-22 15:40:47 +0700

    selesai create page migration

---
+++ database/migrations/2022_03_22_080130_create_detail_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('users_id')->nullable();
+            $table->longText('photo')->nullable();
+            $table->string('role')->nullable(); //jika dia varchar maka dibuat string
+            $table->string('contact_number')->nullable();
+            $table->longText('biography')->nullable(); //jika dia text maka dipakai longText
+            $table->softDeletes();
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_user');
+    }
+}
