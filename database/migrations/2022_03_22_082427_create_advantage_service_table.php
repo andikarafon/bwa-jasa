d3d8257b4a230ceb4878ef4e5e8734c73c499140
andikarafon <andika.rafon.senohaji@gmail.com>
2022-03-24 20:42:02 +0700

    foreign key service

--- database/migrations/2022_03_22_082427_create_advantage_service_table.php
+++ database/migrations/2022_03_22_082427_create_advantage_service_table.php
@@ -15,7 +15,7 @@ class CreateAdvantageServiceTable extends Migration
     {
         Schema::create('advantage_service', function (Blueprint $table) {
             $table->id();
-            $table->integer('service_id')->nullable();
+            $table->foreignId('service_id')->nullable()->index('fk_advantage_service_to_service');
             $table->string('advantage'); //tidak di set nullable artinya harus diisi
             $table->softDeletes();
             $table->timestamps();
