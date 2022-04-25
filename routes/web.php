7382916a21299041daf26f500841286c714f9ecb
andikarafon <andika.rafon.senohaji@gmail.com>
2022-04-25 10:40:49 +0700

    update web.api

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+//daftarkan semua controller yang ada
+
+//front (landing)
+use App\Http\Controllers\Landing\LandingController;
+
+
+//member (dashboard)
+
+use App\Http\Controllers\Dashboard\MemberController;
+use App\Http\Controllers\Dashboard\MyOrderController;
+use App\Http\Controllers\Dashboard\ProfileController;
+use App\Http\Controllers\Dashboard\RequestController;
+use App\Http\Controllers\Dashboard\ServiceController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +28,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+
+
+//dilanding ada page custom maka detailnya harus di atas resource
+Route::get('detail_booking/{id}', LandingController::class, 'detail_booking')->name('detail.booking.landing');
+Route::get('booking/{id}', LandingController::class, 'booking')->name('booking.landing');
+Route::get('detail/{id}', LandingController::class, 'detail')->name('detail.landing');
+Route::get('explore', LandingController::class, 'explore')->name('explore.landing');
+Route::resource('/', LandingController::class);
+
+
+//route group
+Route::group(['prefix' => 'member', 'as' => 'member.', 'middleware' => ['auth:sanctum', 'verified']], //hanya user yang terverifikasi
+function() {
+    
+    //dashboard
+    Route::resource('dashboard', MemberController::class);
+
+    //service
+    Route::resource('service', ServiceController::class);
+
+    //request
+    Route::get('approve_request/{id}', RequestController::class, 'approve')->name('approve.request');
+    Route::resource('request', RequestController::class);
+
+    //myorder
+    Route::get('accept/order/{id}', MyOrderController::class, 'accepted')->name('accept.request');
+    Route::get('reject/order/{id}', MyOrderController::class, 'rejected')->name('reject.request');
+    Route::resource('order', MyOrderController::class);
+
+    //profile
+    Route::get('delete_photo', ProfileController::class, 'delete')->name('delete.photo.profile');
+    Route::resource('profile', ProfileController::class);
+    
+
+});
